621555548f5422eb1fdd7dffe560403c241dae0f
EevaVoipio <eeva.voipio@aalto.fi>
2015-10-27 22:18:25 +0200

    Added total word count, total curse count and pair as requested :), though actually the pair might not be that useful compared to map after all as one still has to call its parameters. Also, currently has lots of commented code on purpose as the development process can be seen from that.

---
+++ src/studio4/Pair.java
@@ -0,0 +1,15 @@
+package studio4;
+
+
+public class Pair {
+
+	public final String word;
+	public final Integer count;
+
+	public Pair(final String word, final Integer count) {
+		this.word = word;
+		this.count = count;
+	}
+
+
+}
